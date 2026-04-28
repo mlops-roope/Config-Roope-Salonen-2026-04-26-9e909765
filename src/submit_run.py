cb3ef2006a87cfd33dcdce40ac14c97724839140
Roope Salonen <roopejuuz@gmail.com>
2026-04-28 12:23:49 +0300

    Revert "Update submit_run.py"
    This reverts commit a10cb7869957ddf494cc2a55a354e596c68196d9.

--- src/submit_run.py
+++ src/submit_run.py
@@ -39,7 +39,6 @@ def submit_pipeline():
         arguments=arguments,
         run_name=run_name,
         experiment_name=experiment_name,
-        pipeline_name="demo-pipeline",
         mode=kfp.dsl.PipelineExecutionMode.V2_COMPATIBLE,
         enable_caching=False,
         namespace="kubeflow-user-example-com"
